5b147249fa4e017f5873112f7a87c518b4ff070b
GlendyT <glendytuyuc@gmail.com>
2024-08-08 12:28:04 -0600

    agregar components, refresh, list

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -7,6 +7,12 @@ import {Animation102Screen} from '../screens/animations/Animation102Screen';
 import {SwitchScreen} from '../screens/switches/SwitchScreen';
 import {AlertScreen} from '../screens/alerts/AlertScreen';
 import {TextInputScreen} from '../screens/inputs/TextInputScreen';
+import {PullToRefreshScreen} from '../screens/ui/PullToRefreshScreen';
+import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
+import { ModalScreen } from '../screens/ui/ModalScreen';
+import { InfiniteScrollScreen } from '../screens/ui/InfiniteScrollScreen';
+import { SlidesScreen } from '../screens/ui/SlidesScreen';
+import { ChangeThemeScreen } from '../screens/ui/ChangeThemeScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,9 +23,17 @@ export const Navigator = () => {
         headerShown: false,
       }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      {/*//TODO: ANIMATION MENU ITEMS */}
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
-
+      {/*//TODO: MENU ITEMS */}
+      <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
+      <Stack.Screen name="CustomSectionListScreen" component={CustomSectionListScreen} />
+      <Stack.Screen name="ModalScreen" component={ModalScreen} />
+      <Stack.Screen name="InfiniteScrollScreen" component={InfiniteScrollScreen} />
+      <Stack.Screen name="SlidesScreen" component={SlidesScreen} />
+      <Stack.Screen name="ChangeThemeScreen" component={ChangeThemeScreen} />
+      {/*//TODO: UI MENU ITEMS */}
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
